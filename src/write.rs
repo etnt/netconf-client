e0d639f28f32f3b066e9d0c06a13d36fdbd10cd6
Torbjorn Tornkvist <kruskakli@gmail.com>
2022-06-03 08:18:20 +0200

    WIP xml pretty printing...

--- src/write.rs
+++ src/write.rs
@@ -1,12 +1,10 @@
 // We need to have the Read/Write traits into our scope
 // so that we can make use of the methods they define.
+use crate::args::Args;
 use crossbeam::channel::Receiver;
 use std::fs::File;
 use std::io::{self, BufWriter, ErrorKind, Result, Write};
-use xml::writer::XmlEvent;
-use xml::{EmitterConfig, EventWriter};
-
-use crate::args::Args;
+use xml::{reader::ParserConfig, writer::EmitterConfig};
 
 pub fn write_loop(args: Args, write_rx: Receiver<Vec<u8>>) -> Result<()> {
     let outfile = &args.outfile;
@@ -22,12 +20,7 @@ pub fn write_loop(args: Args, write_rx: Receiver<Vec<u8>>) -> Result<()> {
             break;
         }
 
-        let mut xml = Vec::new();
-        let mut w = EmitterConfig::default()
-            .perform_indent(true)
-            .create_writer(&mut xml);
-        //let _ = w.write(buffer.as_writer_event());
-        let _ = handle_event(&mut w, buffer.to_s);
+        let mut xml = format_xml(&buffer).unwrap_or_default().as_bytes();
 
         if let Err(e) = writer.write_all(&xml) {
             if e.kind() == ErrorKind::BrokenPipe {
@@ -40,14 +33,21 @@ pub fn write_loop(args: Args, write_rx: Receiver<Vec<u8>>) -> Result<()> {
     Ok(())
 }
 
-fn handle_event<W: Write>(w: &mut EventWriter<W>, line: String) -> Result<()> {
-    let line = line.trim();
-    let event: XmlEvent = if line.starts_with("+") && line.len() > 1 {
-        XmlEvent::start_element(&line[1..]).into()
-    } else if line.starts_with("-") {
-        XmlEvent::end_element().into()
-    } else {
-        XmlEvent::characters(&line).into()
-    };
-    w.write(event)
+fn format_xml(src: &[u8]) -> Result<String> {
+    let mut dest = Vec::new();
+    let reader = ParserConfig::new()
+        .trim_whitespace(true)
+        .ignore_comments(false)
+        .create_reader(src);
+    let mut writer = EmitterConfig::new()
+        .perform_indent(true)
+        .normalize_empty_elements(false)
+        .autopad_comments(false)
+        .create_writer(&mut dest);
+    for event in reader {
+        if let Some(event) = event?.as_writer_event() {
+            writer.write(event).unwrap();
+        }
+    }
+    Ok(String::from_utf8(dest).unwrap())
 }
